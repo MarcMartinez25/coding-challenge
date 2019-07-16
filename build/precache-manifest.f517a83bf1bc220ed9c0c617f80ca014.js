5ce39638aa89dc457f49e4d1a4f184e9c7f67f7b
Marco Martinez <marco@daddledo.lan>
2019-07-16 17:11:37 -0500

    add build

---
+++ build/precache-manifest.f517a83bf1bc220ed9c0c617f80ca014.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800eacb834228213fb3a940a3a54d736",
+    "url": "/index.html"
+  },
+  {
+    "revision": "704b1932503b64732392",
+    "url": "/static/css/main.a4394870.chunk.css"
+  },
+  {
+    "revision": "6836d3f7454d94d836fa",
+    "url": "/static/js/2.5ceed487.chunk.js"
+  },
+  {
+    "revision": "704b1932503b64732392",
+    "url": "/static/js/main.6509599f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
